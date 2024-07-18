1001217a2d1a38e1baf687d59f1c5335fefaf4fd
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-07-18 10:39:22 -0400

    Fixed comments and formatting.

--- accountingledger2/src/main/java/com/pluralsight/AccountingLedgerVersion2/LedgerHome.java
+++ accountingledger2/src/main/java/com/pluralsight/AccountingLedgerVersion2/LedgerHome.java
@@ -15,14 +15,14 @@ public class LedgerHome {
 
         while (running) {
             // Print menu choices.
-            System.out.println("Please choose an option:");
+            System.out.println("\nPlease choose an option:");
             System.out.println("1: View all Transactions.");
             System.out.println("2: View all Deposits.");
             System.out.println("3: View all Payments.");
             System.out.println("4: View Reports.");
             System.out.println("5: Return to Home.");
 
-            // Allow use to enter their choice.
+            // Allow user to enter their choice.
             int choice = input.nextInt();
 
             // Read the user input and execute the appropriate method.
@@ -41,7 +41,6 @@ public class LedgerHome {
                     break;
                 case 5:
                     running = false;
-                    Home.main(null);
                     break;
                 default:
                     System.out.println("Invalid selection.");
